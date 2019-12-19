3f6de20a43d7d10be76cac234709adf699c1f3c5
ymaachi <yassine.maachi@pwc.com>
2019-12-19 11:13:27 +0100

    enabling password change

--- pilot/settings.py
+++ pilot/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'USER': config.DATABASE_USER,
         'HOST': config.DATABASE_HOST,
         'PORT': config.DATABASE_PORT,
+        'PASSWORD': config.DATABASE_PASSWORD,
     }
 }
 
